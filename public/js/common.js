1bd4244391fac072912168ca6069d43168d8cdb2
wzygene <wzygene@126.com>
2017-09-19 23:46:11 +0800

    使用模板引擎渲染了头像

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'], function ($) {
+define(['jquery','template','cookie'], function ($,template) {
 	// NProgress.start();
 	// NProgress.done();
 	
@@ -23,15 +23,18 @@ define(['jquery','cookie'], function ($) {
 
 	// 验证用户是否登录了
 	var flag = $.cookie('PHPSESSID');
-	if (!flag) {
-		// location.href = 'main/login';
-		console.log($.cookie('PHPSESSID'));
+	if (!flag && location.pathname != '/main/login') {
+		location.href = 'main/login';
 	}
 
 
 	// 填充头像信息
 	var loginInfo = $.cookie('loginInfo');
 	loginInfo = loginInfo && JSON.parse(loginInfo);
-	$('.aside .profile img').attr('src', loginInfo.tc_avatar);
-	$('.aside .profile h4').html(loginInfo.tc_name);
+	var tpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+	var html = template.render(tpl,loginInfo);
+	console.log(html);
+	$('.aside .profile').html(html);
+	// $('.aside .profile img').attr('src', loginInfo.tc_avatar);
+	// $('.aside .profile h4').html(loginInfo.tc_name);
 })
