fab960d0307dfb065a4c7bbbb2e93ff9b53122d4
wzygene <wzygene@126.com>
2017-09-19 21:37:44 +0800

    模块化重构项目结构

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,37 @@
-
-	NProgress.start();
-
-	NProgress.done();
-
+define(['jquery','cookie'], function ($) {
+	// NProgress.start();
+	// NProgress.done();
+	
+	// 控制左侧菜单的折叠和展开
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	// 实现退出功能
+	$('#logoutBtn').click(function () {
+		$.ajax({
+			type: 'post',
+			url: 'api/logout',
+			dataType: 'json',
+			success: function (data) {
+				if (data.code == 200) {
+					location.href = 'main/login';
+				}
+			}
+		});
+	});
+
+	// 验证用户是否登录了
+	var flag = $.cookie('PHPSESSID');
+	if (!flag) {
+		// location.href = 'main/login';
+		console.log($.cookie('PHPSESSID'));
+	}
+
+
+	// 填充头像信息
+	var loginInfo = $.cookie('loginInfo');
+	loginInfo = loginInfo && JSON.parse(loginInfo);
+	$('.aside .profile img').attr('src', loginInfo.tc_avatar);
+	$('.aside .profile h4').html(loginInfo.tc_name);
+})
