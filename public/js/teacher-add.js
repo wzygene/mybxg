df19fcc3ccf170b24658c8038393262ee32bda84
wzygene <wzygene@126.com>
2017-09-21 23:45:22 +0800

    完成添加和修改讲师列表信息

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -11,6 +11,7 @@ define(['jquery','template','util'],function ($,template,util){
             success: function (data){
                 var html = template('formTpl',data.result);
                 $('#edit_add').html(html);
+                submitForm('/api/teacher/update');
             }
         });
     }
@@ -18,5 +19,23 @@ define(['jquery','template','util'],function ($,template,util){
         // 添加操作
         var html = template('formTpl');
         $('#edit_add').html(html);
+        submitForm('/api/teacher/add');
+        
+    }
+    function submitForm(url){
+        $('#teacherBtn').click(function (){
+            $.ajax({
+                type: 'post',
+                url: url,
+                data: $('#formBtn').serialize(),
+                dataType: 'json',
+                success: function (data){
+                    console.log($('#formBtn').serialize());
+                    if(data.code == 200){
+                        location.href = '/teacher/list';
+                    }
+                }
+            })
+        });
     }
 });
