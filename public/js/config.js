a3163f7695c2d7dedb587fe30ffd993d31bce124
wzygene <wzygene@126.com>
2017-09-22 17:27:58 +0800

    使用插件完成讲师列表的添加编辑和提交功能

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,15 @@
 require.config({
     baseUrl: '/public/assets',
     paths: {
+        // 这里的文件没有先后顺序之分
         jquery: 'jquery/jquery.min',
         cookie: 'jquery-cookie/jquery.cookie',
+        validate: 'jquery-validate/jquery-validate.min',
+        form: 'jquery-form/jquery.form',
         template: 'artTemplate/template-web',
         bootstrap: 'bootstrap/js/bootstrap.min',
+        datepicker: 'bootstrap-datepicker/js/bootstrap-datepicker',
+        language: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
         util: '../js/util',
         common: '../js/common',
         login: '../js/login',
@@ -13,6 +18,12 @@ require.config({
     },
     shim: {
         bootstrap : {
+            deps : ['jquery']
+        },
+        language: {
+            deps : ['jquery','datepicker']
+        },
+        validate: {
             deps : ['jquery']
         }
     } 
--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function ($,template,util){
+define(['jquery','template','util','datepicker','language','validate','form'],function ($,template,util){
     // 获取 url 中的参数
     var tcId = util.query_param();
     if(tcId){
@@ -22,20 +22,37 @@ define(['jquery','template','util'],function ($,template,util){
         submitForm('/api/teacher/add');
         
     }
+
+    // 基于表单验证插件和表单提交插件进行表单提交
     function submitForm(url){
-        $('#teacherBtn').click(function (){
-            $.ajax({
-                type: 'post',
-                url: url,
-                data: $('#formBtn').serialize(),
-                dataType: 'json',
-                success: function (data){
-                    console.log($('#formBtn').serialize());
-                    if(data.code == 200){
-                        location.href = '/teacher/list';
+        $('#formBtn').validate({
+            sendForm: false,
+            valid: function (){
+                // 提交表单
+                $(this).ajaxSubmit({
+                    type: 'post',
+                    url: url,
+                    dataType: 'json',
+                    success: function (data){
+                        console.log(data);
+                        if (data.code == 200) {
+                            location.href = '/teacher/list';
+                        }
                     }
+                })
+            },
+            description: {
+                tcName: {
+                    required: '用户名不能为空',
+                },
+                tcPass: {
+                    required: '密码不能为空',
+                    pattern: '必须是6位数字',
+                },
+                tcJoinDate: {
+                    required: '日期不能为空'
                 }
-            })
+            }
         });
     }
 });
