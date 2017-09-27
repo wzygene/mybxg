a89546779dba8e4546e8fb68974de92a6ba44dc2
wzygene <wzygene@126.com>
2017-09-27 18:09:57 +0800

    完成课时添加和编辑的模态框重用

--- public/js/courselesson.js
+++ public/js/courselesson.js
@@ -1,21 +1,52 @@
-define(['jquery','template','util'],function ($,template,util){
-    // 设置导航栏按钮
-    util.setMenu('/course/add');
+define(['jquery', 'template', 'util', 'bootstrap'], function ($, template, util) {
+  // 设置导航栏按钮
+  util.setMenu('/course/add');
 
-    // 获取课程id
-    var csId = util.query_param('cs_id');
+  // 获取课程id
+  var csId = util.query_param('cs_id');
 
-    // 获取所有的可是列表
-    $.ajax({
-        type: 'get',
-        url: '/api/course/lesson',
-        data: {cs_id: csId},
-        dataType: 'json',
-        success: function (data){
+  // 获取所有的可视列表
+  $.ajax({
+    type: 'get',
+    url: '/api/course/lesson',
+    data: { cs_id: csId },
+    dataType: 'json',
+    success: function (data) {
+      // 解析数据 渲染页面
+      var html = template('lessonTpl', data.result);
+      $('#lessonInfo').html(html);
+
+      // 处理添加课时操作
+      $('#addLesson').click(function () {
+        var html = template('modalTpl',{operate:'添加课时'});
+        $('#modalInfo').html(html);
+        // 显示弹窗
+        $('#chapterModal').modal();
+      })
+      // 处理课时编辑
+      $('.editLesson').click(function () {
+        // 获取课时id
+        var ctId = $(this).attr('data-ctId');
+        // 发送ajax请求：编辑课时
+        $.ajax({
+          type: 'get',
+          url: '/api/course/chapter/edit',
+          data: { ct_id: ctId },
+          dataType: 'json',
+          success: function (data) {
             console.log(data);
-            // 解析数据 渲染页面
-            var html = template('lessonTpl',data.result);
-            $('#lessonInfo').html(html);
-        }
-    })
+            // 解析数据 渲染模态框
+            data.result.operate = '编辑课时';
+            var html = template('modalTpl',data.result);
+            $('#modalInfo').html(html);
+            $('#chapterModal').modal();
+          }
+        });
+
+      })
+    }
+  });
+
+
+
 })
